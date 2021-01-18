43909306ea1e1f30545b8b420c10fb70b89f4032
Brendan Ferris <brendanfrrs@gmail.com>
2021-01-18 11:36:51 -0500

    initial commit of working but unfinished site

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+app = Flask(__name__)
+
+
+chatbot = ChatBot('Bren', storage_adapter="chatterbot.storage.SQLStorageAdapter")
+# chatbot.storage.drop()
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train("corpus.custom.professionalskills",
+              "corpus.custom.oneliners",
+              "corpus.custom.selfaware",
+              "corpus.custom.help" )
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+     userText = request.args.get("msg")
+     return str(chatbot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
